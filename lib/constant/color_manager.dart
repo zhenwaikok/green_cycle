0b0629f93fccb0c8b37f2a74799b861a7823dcfc
zhenwaikok <kokzhenwai@gmail.com>
2025-06-27 20:49:27 +0800

    add onboarding screen

---
+++ lib/constant/color_manager.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const primary = Color(0xFF187B1B);
+  static const blackColor = Color(0xFF000000);
+  static const whiteColor = Color(0xFFFFFFFF);
+  static const greyColor = Color(0xFF98A2B3);
+  static const redColor = Color(0xFFFF0000);
+}
