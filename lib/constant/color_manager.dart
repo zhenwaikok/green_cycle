90d6e174b80bb4ccc9beefb7c26319b088a2edbd
zhenwaikok <kokzhenwai@gmail.com>
2025-06-29 10:33:23 +0800

    Revert "Dev login signup screen"

--- lib/constant/color_manager.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorManager {
-  static const primary = Color(0xFF187B1B);
-  static const blackColor = Color(0xFF000000);
-  static const whiteColor = Color(0xFFFFFFFF);
-  static const greyColor = Color(0xFF98A2B3);
-  static const redColor = Color(0xFFFF0000);
-}
--- lib/constant/images_manager.dart
+++
@@ -1,5 +0,0 @@
-class Images {
-  static const onboarding1 = 'assets/images/onboarding1.jpg';
-  static const onboarding2 = 'assets/images/onboarding2.png';
-  static const onboarding3 = 'assets/images/onboarding3.jpg';
-}
